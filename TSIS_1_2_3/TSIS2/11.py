52262cf34310cbdfbc67f9ff1018119be8dcd043
Akzhunis Temirkhan <78260466+akzhunistemirkhan@users.noreply.github.com>
2021-06-25 19:18:43 +0600

    Add files via upload

---
+++ TSIS_1_2_3/TSIS2/11.py
@@ -0,0 +1,5 @@
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
---
+++ TSIS_1_2_3/TSIS2/28.py
@@ -0,0 +1,5 @@
+#def my_function(fname, lname):
+ # print(fname)
+
+def my_function(x):
+  return x + 5
